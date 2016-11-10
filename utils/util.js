9dcb5924763faa038e7bc7a03353b796776bf4c9
dnzhu <422298140@qq.com>
2016-11-10 17:34:21 +0800

    update

--- utils/util.js
+++ utils/util.js
@@ -21,48 +21,9 @@ module.exports = {
 }
 
 var index = require('../data/news_data.js')
-//var index_next = require('../data/data_index_next.js')
-//var discovery = require('../data/data_discovery.js')
-//var discovery_next = require('../data/data_discovery_next.js')
-
-function getData(url){
-  return new Promise(function(resolve, reject){
-    wx.request({
-      url: url,
-      data: {},
-      header: {
-        //'Content-Type': 'application/json'
-      },
-      success: function(res) {
-        console.log("success")
-        resolve(res)
-      },
-      fail: function (res) {
-        reject(res)
-        console.log("failed")
-      }
-    })
-  })
-}
 
 function getData2(){
   return index.index;
 }
 
-function getNext(){
-  return index_next.next;
-}
-
-function getDiscovery(){
-  return discovery.discovery;
-}
-
-function discoveryNext(){
-  return discovery_next.next;
-}
-
-function isFunction( obj ) { return typeof obj === 'function'; }
-
-
-module.exports.getData = getData;
 module.exports.getData2 = getData2;
