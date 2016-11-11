030f1ed788061ebfe758dca0325fa601ae011a0f
dnzhu <422298140@qq.com>
2016-11-11 15:40:13 +0800

    update

--- xcar/content/content.js
+++ xcar/content/content.js
@@ -2,7 +2,16 @@
 var app = getApp()
 Page({
   data: {
+    smryHeight:"4.5rem",
+    bookInfo:[],
     art_id: ''
+  },
+  extendBox:function(){
+    if(this.data.smryHeight=="4.5rem"){
+      this.setData({smryHeight:"auto"});
+    }else{
+      this.setData({smryHeight:"4.5rem"});
+    }   
   },
   onLoad: function (options) {
     console.log(options)
@@ -10,8 +19,24 @@ Page({
     //调用应用实例的方法获取全局数据
     that.setData({
         art_id:options.id
-      })
-  }
+      });
+    wx.showToast({
+          title: '加载中',
+          icon: 'loading',
+          duration: 5000
+        });
+    wx.request({
+      url: 'https://api.douban.com/v2/book/'+options.id,
+      success: function(res){
+        // success
+        that.setData({bookInfo:res.data});
+      }
+    });
+  },
+  onReady:function(){
+    // 页面渲染完成
+    wx.hideToast();
+  },
 })
 
 
--- xcar/discovery/discovery.js
+++ xcar/discovery/discovery.js
@@ -1,12 +1,12 @@
 //discovery.js
 const cache_key = 'booklist_101';
 var util = require('../../utils/util.js');
-var app=getApp();
+var app = getApp();
 Page({
   data: {
-    userInfo:{},
-    bookList:[],
-    count:0,
+    userInfo: {},
+    bookList: [],
+    count: 0,
     imgUrls: [
       '../../imgs/scroll1.png',
       '../../imgs/scroll2.png',
@@ -22,35 +22,35 @@ Page({
     console.log('onLoad')
     var that = this
     //用户信息
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     });
     //先取缓存
     wx.getStorage({
       key: cache_key,
-      success: function(res){
+      success: function (res) {
         // success
         console.log(res);
         that.setData({
-            bookList:res.data.books,
-            count:that.data.count+res.data.count
-          });
+          bookList: res.data.books,
+          count: that.data.count + res.data.count
+        });
       },
-      fail: function() {
+      fail: function () {
         // fail，再发送网络请求
         wx.request({
-          url: 'https://api.douban.com/v2/book/series/101/books',     
-          success: function(res) {
-            if(res.data.count==0){
-              return ;
+          url: 'https://api.douban.com/v2/book/series/101/books',
+          success: function (res) {
+            if (res.data.count == 0) {
+              return;
             }
             //渲染到视图
             that.setData({
-              bookList:res.data.books,
-              count:that.data.count+res.data.count
+              bookList: res.data.books,
+              count: that.data.count + res.data.count
             });
             //然后更新或保存到本地缓存
             wx.setStorage({
@@ -58,7 +58,7 @@ Page({
               data: res.data
             })
           },
-          fail:function(res){
+          fail: function (res) {
             console.log(res.data);
             console.log("fail");
           }
@@ -66,34 +66,40 @@ Page({
       }
     });
   },
-  upper: function(e) {
+  upper: function (e) {
     console.log("已到顶部");
   },
-  lower: function(e) {
+  lower: function (e) {
     console.log("已到低部");
-    var that=this;
+    var that = this;
     wx.showToast({
       title: '加载中',
       icon: 'loading',
       duration: 5000
     });
     wx.request({
-          url: 'https://api.douban.com/v2/book/series/120/books',     
-          success: function(res) {
-            if(res.data.count==0){
-              return ;
-            }
-            //渲染到视图
-            that.setData({
-              bookList:that.data.bookList.concat(res.data.books),
-              count:that.data.count+res.data.count
-            });
-            wx.hideToast();
-          },
-          fail:function(res){
-            console.log(res.data);
-            console.log("fail");
-          }
+      url: 'https://api.douban.com/v2/book/series/129/books',
+      method:'GET',
+      data:{count:that.data.count},
+      success: function (res) {
+        if (res.data.count == 0) {
+          return;
+        }
+        //渲染到视图
+        that.setData({
+          bookList: that.data.bookList.concat(res.data.books),
+          count: that.data.count + res.data.count
         });
-  }
+        wx.hideToast();
+      },
+      fail: function (res) {
+        console.log(res.data);
+        console.log("fail");
+      }
+    });
+  },
+  onReady:function(){
+    // 页面渲染完成
+    wx.hideToast();
+  },
 });
